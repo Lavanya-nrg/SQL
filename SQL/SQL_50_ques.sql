0327f07cb65ecc64fca3f15c24eb6b75894f7868
lavanya <lavanya.narang@dataverze.ai>
2024-02-22 12:28:43 +0530

    practicing queries

--- SQL/SQL_50_ques.sql
+++ SQL/SQL_50_ques.sql
@@ -53,3 +53,110 @@ SELECT LTRIM(first_name) FROM WORKER;
 ```sql
 SELECT DISTINCT DEPARTMENT, LENGTH(DEPARTMENT) FROM WORKER;
 ```
+
+9) Write an SQL query to print the FIRST_NAME from Worker table after replacing ‘a’ with ‘A’
+
+**Solution**
+```sql
+SELECT REPLACE(firstname,'a','A') FROM WORKER;
+```
+
+10) Write an SQL query to print the FIRST_NAME and LAST_NAME from Worker table into a single column COMPLETE_NAME.
+-- A space char should separate them.
+
+**Solution**
+```sql
+SELECT CONCAT(first_name,' ', last_name) AS COMPLETE_NAME FROM WORKER;
+```
+
+11) Write an SQL query to print all Worker details from the Worker table order by FIRST_NAME Ascending.
+
+**Solution**
+```sql
+SELECT * FROM WORKER ORDER BY first_name ASC;
+```
+
+12) Write an SQL query to print all Worker details from the Worker table order by 
+-- FIRST_NAME Ascending and DEPARTMENT Descending.
+
+**Solution**
+```sql
+SELECT * FROM WORKER ORDER BY first_name ASC, DEPARTMENT DESC;
+```
+
+13) Write an SQL query to print details for Workers with the first name as “Vipul” and “Satish” from Worker table.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE first_name IN ('Vipul','Satish');
+```
+
+14) Write an SQL query to print details of workers excluding first names, “Vipul” and “Satish” from Worker table.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE first_name NOT IN ('Vipul','Satish');
+```
+
+15) Write an SQL query to print details of Workers with DEPARTMENT name as “Admin*”.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE DEPARTMENT LIKE 'Admin%';
+```
+
+16) Write an SQL query to print details of the Workers whose FIRST_NAME contains ‘a’.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE first_name LIKE '%a%;
+```
+
+17) Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘a’
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE first_name LIKE '%a';
+```
+
+18) Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘h’ and contains six alphabets.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE first_name LIKE '_____h';
+```
+
+19) Write an SQL query to print details of the Workers whose SALARY lies between 100000 and 500000.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE SALARY BETWEEN 100000 and 500000;
+```
+
+20) Write an SQL query to print details of the Workers who have joined in Feb’2014.
+
+**Solution**
+```sql
+SELECT * FROM WORKER WHERE SALARY BETWEEN 100000 and 500000;
+```
+
+21) Write an SQL query to fetch the count of employees working in the department ‘Admin’.
+
+**Solution**
+```sql
+SELECT DEPARTMENT,COUNT(*) FROM WORKER WHERE DEPARTMENT='Admin';
+```
+
+22) Write an SQL query to fetch worker full names with salaries >= 50000 and <= 100000.
+
+**Solution**
+```sql
+SELECT CONCAT(first_name,last_name) FROM WORKER WHERE SALARY BETWEEN 50000 and 100000;
+```
+
+23)  Write an SQL query to fetch the no. of workers for each department in the descending order.
+
+**Solution**
+```sql
+SELECT DEPARTMENT,COUNT(worker_id) FROM WORKER GROUP BY DEPARTMENT ORDER BY DEPARTMENT DESC;
+```
